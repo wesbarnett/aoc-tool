198319171c53529166d0e73f940c0a0599b771a2
Wes Barnett <wes@barnettphd.com>
2023-12-04 20:17:04 -0500

    strip html

--- aoc/__init__.py
+++ aoc/__init__.py
@@ -4,11 +4,35 @@ environment variable AOC_COOKIE.
 """
 
 import os
+import re
+from html.parser import HTMLParser
+from io import StringIO
 from pathlib import Path
 from typing import Any
 from urllib import parse, request
 
 USER_AGENT = "github.com/wesbarnett/aoc-tool by wes@barnettphd.com"
+
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.text = StringIO()
+
+    def handle_data(self, d):
+        self.text.write(d)
+
+    def get_data(self):
+        return self.text.getvalue()
+
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
 
 
 def get_input(year: int, day: int) -> str:
@@ -57,4 +81,6 @@ def submit(answer: Any, year: int, day: int, level: int) -> None:
     data = parse.urlencode({"level": level, "answer": answer}).encode()
     req = request.Request(url, data=data, headers=headers)
     with request.urlopen(req) as response:
-        print(response.read().decode("utf-8"))
+        html = response.read().decode("utf-8")
+    text = strip_tags(re.findall("<article><p>(.*)</p></article>", html)[0])
+    print(text)
